aa56904b8cbc0f5dc3bfc9fdf942e36985320ede
Pedro Castro <contatoaugustopedro@gmail.com>
2023-06-27 11:23:21 -0300

    feat: selecionando um item

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,5 +1,5 @@
 import { ITarefa } from '../../../types/tarefa';
-import style from '../Lista.module.scss';
+import style from './Item.module.scss';
 
 interface Props extends ITarefa{
     selecionaTarefa: (tarefaSelecionada: ITarefa) => void
@@ -14,10 +14,10 @@ export default function Item(
         id, 
         selecionaTarefa 
     }: Props) {
-    console.log('item atual: ', { tarefa, tempo, selecionado, completado, id} );
     return (
         <li 
-            className={style.item} 
+            className={`${style.item} ${selecionado ?  style.
+            itemSelecionado : ''}`} 
             onClick={() => selecionaTarefa(
                 {
                     tarefa,
